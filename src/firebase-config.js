16a755860c645e97a71e5040be216ea38e7f6119
Parker <skagyrin@usetech.ru>
2022-03-11 21:21:51 +0300

    add config firebase file

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGOHdI4VJr06hbiD2ogA68Ow1PFutMN6c",
+  authDomain: "task-calendar-6c313.firebaseapp.com",
+  projectId: "task-calendar-6c313",
+  storageBucket: "task-calendar-6c313.appspot.com",
+  messagingSenderId: "506904117249",
+  appId: "1:506904117249:web:ffb49f9569065ffe2d0000"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
