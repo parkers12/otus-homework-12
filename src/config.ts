4d99bd90767fedfcff9c0ad92858d1a89f4be18a
Parker <skagyrin@usetech.ru>
2022-03-11 15:39:35 +0300

    Add jest and firebase to project

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need 
+import { initializeApp } from "firebase/app"; 
+// TODO: Add SDKs for Firebase products that you want to use 
+// https://firebase.google.com/docs/web/setup#available-libraries   
+// Your web app's Firebase configuration 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional 
+const firebaseConfig = { 
+  apiKey: "AIzaSyCBU6jaGUHfiATwhA8HO1GwwtWg3CuUPHI", 
+  authDomain: "instant-tape-327408.firebaseapp.com", 
+  projectId: "instant-tape-327408", 
+  storageBucket: "instant-tape-327408.appspot.com", 
+  messagingSenderId: "514009193190", 
+  appId: "1:514009193190:web:4fb5b8c5cf1f4119257927", 
+  measurementId: "G-WBQEP5Z0KW" 
+};   
+// Initialize Firebase 
+const app = initializeApp(firebaseConfig); 
