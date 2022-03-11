64554735ac29ff0061206700bc4e517cbb064057
Parker <skagyrin@usetech.ru>
2022-03-11 14:24:51 +0300

    Add files and configurations project

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: ["airbnb-base", "prettier"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: 13,
+      sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+      "import/extensions": ["error"],
+      "max-len": [
+        "error",
+        {
+          ignoreComments: true,
+        },
+      ],
+      "no-unused-vars": "off",
+      "no-use-before-define": "off",
+      "import/prefer-default-export": "off",
+      "no-shadow": "off",
+    },
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".ts", ".js"],
+        },
+      },
+    },
+  };
